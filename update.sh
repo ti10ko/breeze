0633351cdc95ff45fd33140da96f1299bafa65d9
ti10ko <77130277+ti10ko@users.noreply.github.com>
2021-01-08 02:20:42 +0100

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+set -v
+
+export KEYNAME=575796F766AC31A3
+
+(
+    set -e
+    set -v
+
+    cd ./ubuntu/
+
+    # Packages & Packages.gz
+    dpkg-scanpackages --multiversion . > Packages
+    gzip -k -f Packages
+
+    # Release, Release.gpg & InRelease
+    apt-ftparchive release . > Release
+    gpg --default-key "${KEYNAME}" -abs -o - Release > Release.gpg
+    gpg --default-key "${KEYNAME}" --clearsign -o - Release > InRelease
+)
